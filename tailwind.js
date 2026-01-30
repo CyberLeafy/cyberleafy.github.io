8015fb1db17332f1d7e32dabd7bae560c39a68d3
CyberLeafy <dynamo72011@gmail.com>
2026-01-30 17:39:29 +0530

    upgrade and seprate file

---
+++ tailwind.js
@@ -0,0 +1,60 @@
+tailwind.config = {
+            darkMode: 'class',
+            theme: {
+                extend: {
+                    fontFamily: {
+                        sans: ['Inter', 'sans-serif'],
+                    },
+                    colors: {
+                        primary: {
+                            50: '#f0fdf4',
+                            100: '#dcfce7',
+                            200: '#bbf7d0',
+                            300: '#86efac',
+                            400: '#4ade80',
+                            500: '#22c55e',
+                            600: '#16a34a',
+                            700: '#15803d',
+                            800: '#166534',
+                            900: '#14532d',
+                        }
+                    },
+                    animation: {
+                        'blob': 'blob 7s infinite',
+                        'fade-in-up': 'fadeInUp 0.8s ease-out forwards',
+                        'typing': 'typing 4s steps(14) infinite, blink .75s step-end infinite',
+                    },
+                    keyframes: {
+                        blob: {
+                            '0%': { transform: 'translate(0px, 0px) scale(1)' },
+                            '33%': { transform: 'translate(30px, -50px) scale(1.1)' },
+                            '66%': { transform: 'translate(-20px, 20px) scale(0.9)' },
+                            '100%': { transform: 'translate(0px, 0px) scale(1)' },
+                        },
+                        typing: {
+                            '0%, 100%': { width: '0ch' },
+                            '50%, 90%': { width: '10.2ch' },
+                        },
+                        blink: {
+                            '50%': { borderColor: 'transparent' }
+                        },
+                        fadeInUp: {
+                            '0%': { opacity: '0', transform: 'translateY(20px)' },
+                            '100%': { opacity: '1', transform: 'translateY(0)' },
+                        }
+                    }
+                },
+            },
+            plugins: [
+                function ({ addUtilities }) {
+                    addUtilities({
+                        '.zoom-hover': {
+                            transition: 'transform 300ms ease',
+                        },
+                        '.zoom-hover:hover': {
+                            transform: 'scale(1.07)',
+                        },
+                    })
+                },
+            ],
+        }
